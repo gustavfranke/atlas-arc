750c61d63b4b2c8e94f2a42357a5287f3da2f60e
hello <hello@gustavfranke.com>
2026-02-24 13:03:55 +0000

    File changes

--- src/components/site/Navbar.jsx
+++ src/components/site/Navbar.jsx
@@ -40,7 +40,7 @@ export default function Navbar() {
             className="flex items-center"
           >
             <img
-              src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/698dc9d481b400b640a22adf/ac1c9fe29_Logov2.png"
+              src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/698dc9d481b400b640a22adf/7c30c1174_Logov4.png"
               alt="Atlas & Arc"
               className="h-8 md:h-10 w-auto"
             />
