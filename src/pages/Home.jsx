0229ca4bffa26ed5c3d888a6c673d5cfa937475a
hello <hello@gustavfranke.com>
2026-03-21 09:05:50 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,7 +24,6 @@ export default function Home() {
       <WhyUsSection />
       <FeaturedCampaign />
       <PortfolioSection />
-      <TestimonialsSection />
       <CTABanner />
       <ContactSection />
       <Footer />
